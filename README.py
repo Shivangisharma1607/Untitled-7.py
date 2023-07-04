6c8a7604064a4668426ed41431b4dd8d54feee60
Shivangisharma1607 <sshivangi940@gmail.com>
2023-07-04 15:06:47 +0530

    multiply and divide in this python file

--- README.py
+++ README.py
@@ -4,3 +4,11 @@ def add(x, y):
 
 def subtract(x, y):
     return x - y
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Cannot divide by zero."
